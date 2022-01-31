5f26c14cc1b4236553881c4ee6ef95575e1dda66
Serkan <77413501+SerkanUYAR@users.noreply.github.com>
2022-01-31 21:45:57 +0300

    Add project files.

---
+++ Captcha Oluşturma/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captcha_Oluşturma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (label1.Text == textBox1.Text)
+            {
+                MessageBox.Show(" İŞLEM BAŞARILI","BİLGİ",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("İŞLEM BAŞARISIZ", "BİLGİ",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] sembol1 = { "a", "b", "c", "d", "e", "f", "g" };
+            string[] sembol2 = { "+", "-", "/", "+", "&" };
+            string[] sembol3 = { "A", "B", "C", "D", "E" };
+
+            Random r = new Random();
+            int s1, s2, s3,s4;
+            s1 = r.Next(0, sembol1.Length);
+            s2 = r.Next(0, sembol2.Length);
+            s3 = r.Next(1, 10);
+            s4 = r.Next(0, sembol3.Length);
+            label1.Text = sembol1[s1].ToString() + sembol2[s2].ToString() + s3.ToString()+sembol3[s4].ToString();
+        }
+    }
+}
---
+++ Captcha Oluşturma/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Captcha Oluşturma")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Captcha Oluşturma")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("e7002803-b608-43eb-844a-d0dd5964133e")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
